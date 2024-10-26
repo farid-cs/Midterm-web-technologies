ad5d3584fa9f837f4c9b5b30fe652484ef3d7967
Farid <faridfaracli9@gmail.com>
2024-10-27 01:40:51 +0400

    Implement functionality for some of the buttons

---
+++ script.js
@@ -0,0 +1,24 @@
+const buttons = document.getElementsByTagName('button');
+let input = document.getElementsByClassName('input')[0];
+
+function callback(ev) {
+	let text = ev.target.innerText;
+
+	switch (text) {
+	case '=':
+		input.innerText = 'Result';
+		break;
+	case '←':
+		input.innerText = input.innerText.slice(0, -1);
+		break;
+	case 'C':
+		input.innerText = '';
+		break;
+	default:
+		input.innerText += text;
+	}
+}
+
+for (const button of buttons) {
+	button.addEventListener('click', callback);
+}
